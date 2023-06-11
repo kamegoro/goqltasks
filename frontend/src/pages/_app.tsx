9c93132e45e8491a55eea9f32c68298b36251d83
kame <tentyo0620@icloud.com>
2023-06-11 13:20:45 +0900

    add reset css

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import type { AppProps } from 'next/app';
 
+import '@/styles/globals.css';
+
 import ApolloWrapper from '@/components/ApolloWrapper';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
