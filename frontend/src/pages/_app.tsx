6d5818a4560cca196031a00cfc68aca96fd7f5d1
kame <tentyo0620@icloud.com>
2023-06-11 12:51:57 +0900

    create front top

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import type { AppProps } from 'next/app';
 
 import ApolloWrapper from '@/components/ApolloWrapper';
-import '@/styles/globals.css';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next';
 
 import { useQuery } from '@apollo/client';
 
-import { GetTodoDocument } from '../graphql/dist/client';
-import { GetTodoQuery } from '../graphql/dist/client';
+import { GetTodoDocument } from '@/generated/graphql';
+import { GetTodoQuery } from '@/generated/graphql';
 
 const Home: NextPage = () => {
   const { data } = useQuery<GetTodoQuery>(GetTodoDocument);
